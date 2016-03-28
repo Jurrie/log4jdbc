d0a1e421cdcfc5d7dcf1eca11016c11ea3eababd
Jurrie Overgoor <jurrie.overgoor@finan.nl>
2016-03-28 11:02:00 +0200

    Add Oracle XA support for WebSphere
    All public getters and setters from oracle.jdbc.xa.client.OracleXADataSource are implemented using reflection. This allows WebSphere to set properties using reflection. Oracle XA is now confirmed to work on WAS8.

--- src/main/java/net/sf/log4jdbc/xa/Oracle.java
+++ src/main/java/net/sf/log4jdbc/xa/Oracle.java
@@ -1,5 +1,8 @@
 package net.sf.log4jdbc.xa;
 
+import java.io.PrintWriter;
+import java.util.Properties;
+
 public class Oracle extends XADataSourceSpy
 {
 	public Oracle() throws ClassNotFoundException, InstantiationException, IllegalAccessException
@@ -7,13 +10,242 @@ public class Oracle extends XADataSourceSpy
 		super("oracle.jdbc.xa.client.OracleXADataSource");
 	}
 
-	public synchronized String getURL()
+	public boolean getNativeXA()
+	{
+		return invokeMethod("getNativeXA");
+	}
+
+	public void setNativeXA(boolean nativeXA)
+	{
+		invokeMethod("setNativeXA", new Object[] { nativeXA }, new Class<?>[] { boolean.class });
+	}
+
+	public String getConnectionCacheName()
+	{
+		return invokeMethod("getConnectionCacheName");
+	}
+
+	public void setConnectionCacheName(String connectionCacheName)
+	{
+		invokeMethod("setConnectionCacheName", connectionCacheName);
+	}
+
+	public Properties getConnectionCacheProperties()
+	{
+		return invokeMethod("getConnectionCacheProperties");
+	}
+
+	public void setConnectionCacheProperties(Properties connectionCacheProperties)
+	{
+		invokeMethod("setConnectionCacheProperties", connectionCacheProperties);
+	}
+
+	public boolean getConnectionCachingEnabled()
+	{
+		return invokeMethod("getConnectionCachingEnabled");
+	}
+
+	public void setConnectionCachingEnabled(boolean connectionCachingEnabled)
+	{
+		invokeMethod("setConnectionCachingEnabled", new Object[] { connectionCachingEnabled }, new Class<?>[] { boolean.class });
+	}
+
+	public Properties getConnectionProperties()
+	{
+		return invokeMethod("getConnectionProperties");
+	}
+
+	public void setConnectionProperties(Properties connectionProperties)
+	{
+		invokeMethod("setConnectionProperties", connectionProperties);
+	}
+
+	public String getDatabaseName()
+	{
+		return invokeMethod("getDatabaseName");
+	}
+
+	public void setDatabaseName(String databaseName)
+	{
+		invokeMethod("setDatabaseName", databaseName);
+	}
+
+	public String getDataSourceName()
+	{
+		return invokeMethod("getDataSourceName");
+	}
+
+	public void setDataSourceName(String dataSourceName)
+	{
+		invokeMethod("setDataSourceName", dataSourceName);
+	}
+
+	public String getDescription()
+	{
+		return invokeMethod("getDescription");
+	}
+
+	public void setDescription(String description)
+	{
+		invokeMethod("setDescription", description);
+	}
+
+	public String getDriverType()
+	{
+		return invokeMethod("getDriverType");
+	}
+
+	public void setDriverType(String driverType)
+	{
+		invokeMethod("setDriverType", driverType);
+	}
+
+	public boolean getExplicitCachingEnabled()
+	{
+		return invokeMethod("getExplicitCachingEnabled");
+	}
+
+	public void setExplicitCachingEnabled(boolean explicitCachingEnabled)
+	{
+		invokeMethod("setExplicitCachingEnabled", new Object[] { explicitCachingEnabled }, new Class<?>[] { boolean.class });
+	}
+
+	public boolean getFastConnectionFailoverEnabled()
+	{
+		return invokeMethod("getFastConnectionFailoverEnabled");
+	}
+
+	public void setFastConnectionFailoverEnabled(boolean fastConnectionFailoverEnabled)
+	{
+		invokeMethod("setFastConnectionFailoverEnabled", new Object[] { fastConnectionFailoverEnabled }, new Class<?>[] { boolean.class });
+	}
+
+	public boolean getImplicitCachingEnabled()
+	{
+		return invokeMethod("getImplicitCachingEnabled");
+	}
+
+	public void setImplicitCachingEnabled(boolean implicitCachingEnabled)
+	{
+		invokeMethod("setImplicitCachingEnabled", new Object[] { implicitCachingEnabled }, new Class<?>[] { boolean.class });
+	}
+
+	@Override
+	public int getLoginTimeout()
+	{
+		return invokeMethod("getLoginTimeout");
+	}
+
+	@Override
+	public void setLoginTimeout(int loginTimeout)
+	{
+		invokeMethod("setLoginTimeout", new Object[] { loginTimeout }, new Class<?>[] { int.class });
+	}
+
+	@Override
+	public PrintWriter getLogWriter()
+	{
+		return invokeMethod("logWriter");
+	}
+
+	@Override
+	public void setLogWriter(PrintWriter logWriter)
+	{
+		invokeMethod("logWriter", logWriter);
+	}
+
+	public int getMaxStatements()
+	{
+		return invokeMethod("getMaxStatements");
+	}
+
+	public void setMaxStatements(int maxStatements)
+	{
+		invokeMethod("setMaxStatements", new Object[] { maxStatements }, new Class<?>[] { int.class });
+	}
+
+	public String getNetworkProtocol()
+	{
+		return invokeMethod("getNetworkProtocol");
+	}
+
+	public void setNetworkProtocol(String networkProtocol)
+	{
+		invokeMethod("setNetworkProtocol", networkProtocol);
+	}
+
+	public String getONSConfiguration()
+	{
+		return invokeMethod("getONSConfiguration");
+	}
+
+	public void setONSConfiguration(String onsConfiguration)
+	{
+		invokeMethod("setONSConfiguration", onsConfiguration);
+	}
+
+	public int getPortNumber()
+	{
+		return invokeMethod("getPortNumber");
+	}
+
+	public void setPortNumber(int portNumber)
+	{
+		invokeMethod("setPortNumber", new Object[] { portNumber }, new Class<?>[] { int.class });
+	}
+
+	public void setPassword(String password)
+	{
+		invokeMethod("setPassword", password);
+	}
+
+	public String getServerName()
+	{
+		return invokeMethod("getServerName");
+	}
+
+	public void setServerName(String serverName)
+	{
+		invokeMethod("setServerName", serverName);
+	}
+
+	public String getServiceName()
+	{
+		return invokeMethod("getServiceName");
+	}
+
+	public void setServiceName(String serviceName)
+	{
+		invokeMethod("setServiceName", serviceName);
+	}
+
+	public String getTNSEntryName()
+	{
+		return invokeMethod("getTNSEntryName");
+	}
+
+	public void setTNSEntryName(String tnsEntryName)
+	{
+		invokeMethod("setTNSEntryName", tnsEntryName);
+	}
+
+	public String getURL()
 	{
 		return invokeMethod("getURL");
 	}
 
-	public synchronized void setURL(String url)
+	public void setURL(String url)
 	{
 		invokeMethod("setURL", url);
+	}
+
+	public String getUser()
+	{
+		return invokeMethod("getUser");
+	}
+
+	public void setUser(String user)
+	{
+		invokeMethod("setUser", user);
 	}
 }
