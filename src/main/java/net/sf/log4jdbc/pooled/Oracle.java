8962580a93cc866a3b6b3d64298e11027488c516
Jurrie Overgoor <jurrie.overgoor@finan.nl>
2016-03-28 18:20:09 +0200

    Add support for PostgreSQL and Oracle-pooled.
    This is still untested.

--- src/main/java/net/sf/log4jdbc/pooled/Oracle.java
+++ src/main/java/net/sf/log4jdbc/pooled/Oracle.java
@@ -1,9 +1,216 @@
 package net.sf.log4jdbc.pooled;
 
+import java.util.Properties;
+
 public class Oracle extends ConnectionPoolDataSourceSpy
 {
 	public Oracle() throws InstantiationException, IllegalAccessException, ClassNotFoundException
 	{
 		super("oracle.jdbc.pool.OracleConnectionPoolDataSource");
+	}
+
+	public String getConnectionCacheName()
+	{
+		return invokeMethod("getConnectionCacheName");
+	}
+
+	public void setConnectionCacheName(String connectionCacheName)
+	{
+		invokeMethod("setConnectionCacheName", connectionCacheName);
+	}
+
+	public Properties getConnectionCacheProperties()
+	{
+		return invokeMethod("getConnectionCacheProperties");
+	}
+
+	public void setConnectionCacheProperties(Properties connectionCacheProperties)
+	{
+		invokeMethod("setConnectionCacheProperties", connectionCacheProperties);
+	}
+
+	public boolean getConnectionCachingEnabled()
+	{
+		return invokeMethod("getConnectionCachingEnabled");
+	}
+
+	public void setConnectionCachingEnabled(boolean connectionCachingEnabled)
+	{
+		invokeMethod("setConnectionCachingEnabled", new Object[] { connectionCachingEnabled }, new Class<?>[] { boolean.class });
+	}
+
+	public Properties getConnectionProperties()
+	{
+		return invokeMethod("getConnectionProperties");
+	}
+
+	public void setConnectionProperties(Properties connectionProperties)
+	{
+		invokeMethod("setConnectionProperties", connectionProperties);
+	}
+
+	public String getDatabaseName()
+	{
+		return invokeMethod("getDatabaseName");
+	}
+
+	public void setDatabaseName(String databaseName)
+	{
+		invokeMethod("setDatabaseName", databaseName);
+	}
+
+	public String getDataSourceName()
+	{
+		return invokeMethod("getDataSourceName");
+	}
+
+	public void setDataSourceName(String dataSourceName)
+	{
+		invokeMethod("setDataSourceName", dataSourceName);
+	}
+
+	public String getDescription()
+	{
+		return invokeMethod("getDescription");
+	}
+
+	public void setDescription(String description)
+	{
+		invokeMethod("setDescription", description);
+	}
+
+	public String getDriverType()
+	{
+		return invokeMethod("getDriverType");
+	}
+
+	public void setDriverType(String driverType)
+	{
+		invokeMethod("setDriverType", driverType);
+	}
+
+	public boolean getExplicitCachingEnabled()
+	{
+		return invokeMethod("getExplicitCachingEnabled");
+	}
+
+	public void setExplicitCachingEnabled(boolean explicitCachingEnabled)
+	{
+		invokeMethod("setExplicitCachingEnabled", new Object[] { explicitCachingEnabled }, new Class<?>[] { boolean.class });
+	}
+
+	public boolean getFastConnectionFailoverEnabled()
+	{
+		return invokeMethod("getFastConnectionFailoverEnabled");
+	}
+
+	public void setFastConnectionFailoverEnabled(boolean fastConnectionFailoverEnabled)
+	{
+		invokeMethod("setFastConnectionFailoverEnabled", new Object[] { fastConnectionFailoverEnabled }, new Class<?>[] { boolean.class });
+	}
+
+	public boolean getImplicitCachingEnabled()
+	{
+		return invokeMethod("getImplicitCachingEnabled");
+	}
+
+	public void setImplicitCachingEnabled(boolean implicitCachingEnabled)
+	{
+		invokeMethod("setImplicitCachingEnabled", new Object[] { implicitCachingEnabled }, new Class<?>[] { boolean.class });
+	}
+
+	public int getMaxStatements()
+	{
+		return invokeMethod("getMaxStatements");
+	}
+
+	public void setMaxStatements(int maxStatements)
+	{
+		invokeMethod("setMaxStatements", new Object[] { maxStatements }, new Class<?>[] { int.class });
+	}
+
+	public String getNetworkProtocol()
+	{
+		return invokeMethod("getNetworkProtocol");
+	}
+
+	public void setNetworkProtocol(String networkProtocol)
+	{
+		invokeMethod("setNetworkProtocol", networkProtocol);
+	}
+
+	public String getONSConfiguration()
+	{
+		return invokeMethod("getONSConfiguration");
+	}
+
+	public void setONSConfiguration(String onsConfiguration)
+	{
+		invokeMethod("setONSConfiguration", onsConfiguration);
+	}
+
+	public int getPortNumber()
+	{
+		return invokeMethod("getPortNumber");
+	}
+
+	public void setPortNumber(int portNumber)
+	{
+		invokeMethod("setPortNumber", new Object[] { portNumber }, new Class<?>[] { int.class });
+	}
+
+	public void setPassword(String password)
+	{
+		invokeMethod("setPassword", password);
+	}
+
+	public String getServerName()
+	{
+		return invokeMethod("getServerName");
+	}
+
+	public void setServerName(String serverName)
+	{
+		invokeMethod("setServerName", serverName);
+	}
+
+	public String getServiceName()
+	{
+		return invokeMethod("getServiceName");
+	}
+
+	public void setServiceName(String serviceName)
+	{
+		invokeMethod("setServiceName", serviceName);
+	}
+
+	public String getTNSEntryName()
+	{
+		return invokeMethod("getTNSEntryName");
+	}
+
+	public void setTNSEntryName(String tnsEntryName)
+	{
+		invokeMethod("setTNSEntryName", tnsEntryName);
+	}
+
+	public String getURL()
+	{
+		return invokeMethod("getURL");
+	}
+
+	public void setURL(String url)
+	{
+		invokeMethod("setURL", url);
+	}
+
+	public String getUser()
+	{
+		return invokeMethod("getUser");
+	}
+
+	public void setUser(String user)
+	{
+		invokeMethod("setUser", user);
 	}
 }
--- src/main/java/net/sf/log4jdbc/pooled/PostgreSQL.java
+++ src/main/java/net/sf/log4jdbc/pooled/PostgreSQL.java
@@ -6,4 +6,249 @@ public class PostgreSQL extends ConnectionPoolDataSourceSpy
 	{
 		super("org.postgresql.ds.PGConnectionPoolDataSource");
 	}
+
+	public boolean isDefaultAutoCommit()
+	{
+		return invokeMethod("isDefaultAutoCommit");
+	}
+
+	public void setDefaultAutoCommit(boolean defaultAutoCommit)
+	{
+		invokeMethod("setDefaultAutoCommit", new Object[] { defaultAutoCommit }, new Class<?>[] { boolean.class });
+	}
+
+	public String getApplicationName()
+	{
+		return invokeMethod("getApplicationName");
+	}
+
+	public void setApplicationName(String applicationName)
+	{
+		invokeMethod("setApplicationName", applicationName);
+	}
+
+	public boolean getBinaryTransfer()
+	{
+		return invokeMethod("getBinaryTransfer");
+	}
+
+	public void setBinaryTransfer(boolean binaryTransfer)
+	{
+		invokeMethod("setBinaryTransfer", new Object[] { binaryTransfer }, new Class<?>[] { boolean.class });
+	}
+
+	public String getBinaryTransferDisable()
+	{
+		return invokeMethod("getBinaryTransferDisable");
+	}
+
+	public void setBinaryTransferDisable(String binaryTransferDisable)
+	{
+		invokeMethod("setBinaryTransferDisable", binaryTransferDisable);
+	}
+
+	public String getBinaryTransferEnable()
+	{
+		return invokeMethod("getBinaryTransferEnable");
+	}
+
+	public void setBinaryTransferEnable(String binaryTransferEnable)
+	{
+		invokeMethod("setBinaryTransferEnable", binaryTransferEnable);
+	}
+
+	public String getCompatible()
+	{
+		return invokeMethod("getCompatible");
+	}
+
+	public void setCompatible(String compatible)
+	{
+		invokeMethod("setCompatible", compatible);
+	}
+
+	public String getDatabaseName()
+	{
+		return invokeMethod("getDatabaseName");
+	}
+
+	public void setDatabaseName(String databaseName)
+	{
+		invokeMethod("setDatabaseName", databaseName);
+	}
+
+	public String getDescription()
+	{
+		return invokeMethod("getDescription");
+	}
+
+	public boolean isColumnSanitiserDisabled()
+	{
+		return invokeMethod("isColumnSanitiserDisabled");
+	}
+
+	public void setDisableColumnSanitiser(boolean disableColumnSanitiser)
+	{
+		invokeMethod("setDisableColumnSanitiser", new Object[] { disableColumnSanitiser }, new Class<?>[] { int.class });
+	}
+
+	public int getLogLevel()
+	{
+		return invokeMethod("getLogLevel");
+	}
+
+	public void setLogLevel(int logLevel)
+	{
+		invokeMethod("setLogLevel", new Object[] { logLevel }, new Class<?>[] { int.class });
+	}
+
+	public String getPassword()
+	{
+		return invokeMethod("getPassword");
+	}
+
+	public void setPassword(String password)
+	{
+		invokeMethod("setPassword", password);
+	}
+
+	public int getPortNumber()
+	{
+		return invokeMethod("getPortNumber");
+	}
+
+	public void setPortNumber(int portNumber)
+	{
+		invokeMethod("setPortNumber", new Object[] { portNumber }, new Class<?>[] { int.class });
+	}
+
+	public int getPrepareThreshold()
+	{
+		return invokeMethod("getPrepareThreshold");
+	}
+
+	public void setPrepareThreshold(int prepareThreshold)
+	{
+		invokeMethod("setPrepareThreshold", new Object[] { prepareThreshold }, new Class<?>[] { int.class });
+	}
+
+	public int getProtocolVersion()
+	{
+		return invokeMethod("getProtocolVersion");
+	}
+
+	public void setProtocolVersion(int protocolVersion)
+	{
+		invokeMethod("setProtocolVersion", new Object[] { protocolVersion }, new Class<?>[] { int.class });
+	}
+
+	public int getReceiveBufferSize()
+	{
+		return invokeMethod("getReceiveBufferSize");
+	}
+
+	public void setReceiveBufferSize(int receiveBufferSize)
+	{
+		invokeMethod("setReceiveBufferSize", new Object[] { receiveBufferSize }, new Class<?>[] { int.class });
+	}
+
+	public int getSendBufferSize()
+	{
+		return invokeMethod("getSendBufferSize");
+	}
+
+	public void setSendBufferSize(int sendBufferSize)
+	{
+		invokeMethod("setSendBufferSize", new Object[] { sendBufferSize }, new Class<?>[] { int.class });
+	}
+
+	public String getServerName()
+	{
+		return invokeMethod("getServerName");
+	}
+
+	public void setServerName(String serverName)
+	{
+		invokeMethod("setServerName", serverName);
+	}
+
+	public int getSocketTimeout()
+	{
+		return invokeMethod("getSocketTimeout");
+	}
+
+	public void setSocketTimeout(int socketTimeout)
+	{
+		invokeMethod("setSocketTimeout", new Object[] { socketTimeout }, new Class<?>[] { int.class });
+	}
+
+	public boolean getSsl()
+	{
+		return invokeMethod("getSsl");
+	}
+
+	public void setSsl(boolean ssl)
+	{
+		invokeMethod("setSsl", new Object[] { ssl }, new Class<?>[] { boolean.class });
+	}
+
+	public String getSslfactory()
+	{
+		return invokeMethod("getSslfactory");
+	}
+
+	public void setSslfactory(String sslfactory)
+	{
+		invokeMethod("setSslfactory", sslfactory);
+	}
+
+	public String getStringType()
+	{
+		return invokeMethod("getStringType");
+	}
+
+	public void setStringType(String stringType)
+	{
+		invokeMethod("setStringType", stringType);
+	}
+
+	public boolean getTcpKeepAlive()
+	{
+		return invokeMethod("getTcpKeepAlive");
+	}
+
+	public void setTcpKeepAlive(boolean tcpKeepAlive)
+	{
+		invokeMethod("setTcpKeepAlive", new Object[] { tcpKeepAlive }, new Class<?>[] { boolean.class });
+	}
+
+	public int getUnknownLength()
+	{
+		return invokeMethod("getUnknownLength");
+	}
+
+	public void setUnknownLength(int unknownLength)
+	{
+		invokeMethod("setUnknownLength", new Object[] { unknownLength }, new Class<?>[] { int.class });
+	}
+
+	public String getUrl()
+	{
+		return invokeMethod("getUrl");
+	}
+
+	public void setUrl(String url)
+	{
+		invokeMethod("setUrl", url);
+	}
+
+	public String getUser()
+	{
+		return invokeMethod("getUser");
+	}
+
+	public void setUser(String user)
+	{
+		invokeMethod("setUser", user);
+	}
 }
